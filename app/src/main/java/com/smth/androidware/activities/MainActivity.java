6217cfe83eb5df2286f42f1ffa5077bb4efc4d49
sorina-vasiliu <vasiliu.sorina@gmail.com>
2016-07-15 03:31:09 +0300

    Added main activity to have access to the rest of activities + sample of toolbar used as an actionbar

---
+++ app/src/main/java/com/smth/androidware/activities/MainActivity.java
@@ -0,0 +1,67 @@
+package com.smth.androidware.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.smth.androidware.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.goToToolbarBtn)
+    Button goToToolbarBtn;
+
+    @BindView(R.id.goToRecyclerViewBtn)
+    Button goToRecyclerViewBtn;
+
+    @BindView(R.id.goToNavigationDrawer)
+    Button goToNavigationDrawer;
+
+    @BindView(R.id.goToAnimations)
+    Button goToAnimations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        goToToolbarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ToolbarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goToRecyclerViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goToNavigationDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NavigationDrawerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goToAnimations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AnimationsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/smth/androidware/activities/ToolbarActivity.java
+++ app/src/main/java/com/smth/androidware/activities/ToolbarActivity.java
@@ -3,6 +3,7 @@ package com.smth.androidware.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -44,25 +45,34 @@ public class ToolbarActivity extends AppCompatActivity {
         toolbar.setTitle("Thiscounts!");
         toolbar.setLogo(R.mipmap.ic_logo);
         toolbar.inflateMenu(R.menu.menu_main);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
+        //Toolbar is used here as an action bar to navigate between activities.
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-                String msg = "";
-                switch (item.getItemId()){
-                    case R.id.settings:
-                        msg = "Settings";
-                        break;
-                    case R.id.search:
-                        msg = "Search";
-                        break;
-                }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
 
-                Toast.makeText(ToolbarActivity.this,msg,Toast.LENGTH_SHORT).show();
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
                 return true;
-            }
-        });
+            case R.id.settings:
+                msg = "Settings";
+                break;
+            case R.id.search:
+                msg = "Search";
+                break;
+        }
+        Toast.makeText(ToolbarActivity.this,msg,Toast.LENGTH_SHORT).show();
+        return true;
     }
 
 }
